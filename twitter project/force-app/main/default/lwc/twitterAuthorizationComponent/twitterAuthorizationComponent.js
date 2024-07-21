d9e6ef794aadbcb772dea55636a3669212e3ec0b
VovaZrovko <vovazrovko380969842477@gmail.com>
2024-07-22 02:21:12 +0300

    updated twitter authorization component to use data fro, custom settings

--- twitter project/force-app/main/default/lwc/twitterAuthorizationComponent/twitterAuthorizationComponent.js
+++ twitter project/force-app/main/default/lwc/twitterAuthorizationComponent/twitterAuthorizationComponent.js
@@ -1,12 +1,16 @@
 import { LightningElement, wire } from 'lwc';
 import getTwitterUserSettings from '@salesforce/apex/TwitterAuthController.getTwitterUserSettings';
+import getTwitterAppConfig from '@salesforce/apex/TwitterAuthController.getTwitterAppConfig';
 
 const SPLITSYMBOL = '~';
+const AUTHORIZEURL = 'https://twitter.com/i/oauth2/authorize?';
 
 export default class TwitterAuthorizationComponent extends LightningElement {
     twitterUserSetting;
     username;
     expirationDateTime;
+    redirectUri;
+    clientId;
 
     get cardTitle() {
         return 'You are authorized as ' + this.username;
@@ -22,25 +26,27 @@ export default class TwitterAuthorizationComponent extends LightningElement {
         return new Date(this.expirationDateTime).toLocaleString();
     }
 
+    async connectedCallback() {
+        let config = await getTwitterAppConfig();
+        [ this.redirectUri, this.clientId ] = config.split(SPLITSYMBOL);
+    }
+
     handleAuthorization() {
-        const clientId = 'T3N2bG5JcnB2S09JWHI4YlVHSWE6MTpjaQ';
-        const redirectUri = 'https://resilient-wolf-fqzlz-dev-ed--c.vf.force.com/apex/intermidiateIntegrationVFPage';
         const responseType = 'code';
         const scope = 'tweet.read tweet.write users.read offline.access';
         const state = 'state';
         const challenge = 'challenge';
         const plain = 'plain';
-        
-        const authorizationUrl = `https://twitter.com/i/oauth2/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}&state=${state}&code_challenge=${challenge}&code_challenge_method=${plain}`;
-        console.log();
+        const authorizationUrl = AUTHORIZEURL + `response_type=${responseType}&client_id=${this.clientId}&redirect_uri=${encodeURIComponent(this.redirectUri)}&scope=${encodeURIComponent(scope)}&state=${state}&code_challenge=${challenge}&code_challenge_method=${plain}`;
+
         window.location.href = authorizationUrl;
     }
 
     @wire(getTwitterUserSettings)
     getTwitterUserSettings(result) {
         this.twitterUserSetting = result;
+
         if(result.data) {
-            console.log('result.data ' + result.data);
             [ this.username, this.expirationDateTime ] = result.data.split(SPLITSYMBOL);
         }
     }
